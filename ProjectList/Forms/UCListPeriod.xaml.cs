777070c6d5b29d7d4c905bab8e503ae66c8dce84
Kon2007 <konors2007@ya.ru>
2025-06-19 21:37:08 +0400

    Add files via upload

---
+++ ProjectList/Forms/UCListPeriod.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MSProject = Microsoft.Office.Interop.MSProject;
+
+namespace ProjectList.Forms
+{
+    /// <summary>
+    /// Логика взаимодействия для UCListPeriod.xaml
+    /// </summary>
+    public partial class UCListPeriod : System.Windows.Controls.UserControl
+    {
+        public Form Form;
+        public MSProject.Task _task;
+        public UCListPeriod()
+        {
+            InitializeComponent();
+
+            ListPeriodTask listPeriodTask = new ListPeriodTask();
+            
+            DataContext = listPeriodTask;
+
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is ListPeriodTask listPeriodTask) listPeriodTask.UpLoad(_task);
+        }
+    }
+}
---
+++ ProjectList/MVVM/ModelTask.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.Office.Core;
+using Microsoft.Office.Interop.MSProject;
+using ProjectList.MVVM;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.AxHost;
+using MSProject = Microsoft.Office.Interop.MSProject;
+
+namespace ProjectList
+{
+
+    internal class ListPeriodTask: MyINotifyPropertyChanged
+    {
+        private DateTime minDate;
+        private DateTime maxDate;
+        
+        private TaskPeriod selectedtaskPeriod;
+
+        public TaskPeriod SelectedTaskPeriod 
+        { 
+            get { return selectedtaskPeriod; }
+            set 
+            { 
+                selectedtaskPeriod = value; 
+                OnPropertyChanged(nameof(SelectedTaskPeriod));
+            }
+        }
+
+        public ObservableCollection<TaskPeriod> TaskPeriods { get; set; }
+
+        private MSProject.Task task; 
+
+        public ListPeriodTask() 
+        {
+            minDate = DateTime.MaxValue;
+            maxDate = DateTime.MinValue;
+
+            TaskPeriods = new ObservableCollection<TaskPeriod>();
+        }
+
+        public void UpLoad(MSProject.Task _task) 
+        {
+            task = _task;
+
+            TaskPeriods.Clear();
+
+            if (task != null)
+                foreach (SplitPart part in (MSProject.SplitParts)task.SplitParts)
+                {
+                    DateTime start = (DateTime)part.Start;
+                    DateTime finish = (DateTime)part.Finish;
+
+                    if (minDate > start) minDate = start;
+                    if (maxDate < finish) maxDate = finish;
+
+                    TaskPeriods.Add(new TaskPeriod(start, finish));
+                }
+
+
+        }
+
+
+        private RelayCommand saveCommand;
+
+        public RelayCommand SaveCommand => saveCommand ??= new RelayCommand(obj => SaveTaskPeriod());
+
+        private void SaveTaskPeriod() 
+        {
+            if (task != null)
+            {
+
+                foreach (TaskPeriod period in TaskPeriods)
+                {
+                    if (minDate > period.DataStart) minDate = period.DataStart;
+                    if (maxDate < period.DataStop) maxDate = period.DataStop;
+                }
+
+                // Сначала удалить
+                for (int i = task.SplitParts.Count; i >= 1; i--)
+                {
+                    task.SplitParts[i].Delete();
+                }
+
+                task.Start = minDate;
+                task.Finish = maxDate;
+
+
+                // Затем новые создать
+                foreach (TaskPeriod newPeriod in TaskPeriods)
+                {
+                    //task.SplitParts.Add(newPeriod.DataStart, newPeriod.DataStop);
+                    task.Split(newPeriod.DataStart, newPeriod.DataStop);
+                }
+                task.ConstraintType = 0;
+                task.ConstraintDate = "НД";
+
+            }
+
+        }
+    }
+
+    internal class TaskPeriod: MyINotifyPropertyChanged
+    {
+        private DateTime dataStart;
+
+        private DateTime dataStop;
+
+        public DateTime DataStart 
+        {   get { return dataStart; }
+            set 
+            { 
+                dataStart = value;
+                OnPropertyChanged(nameof(dataStart));
+            } 
+        }
+        public DateTime DataStop
+        {
+            get {  return dataStop; }
+            set 
+            { 
+                dataStop = value; 
+                OnPropertyChanged(nameof(dataStop));
+            } 
+        }
+
+        public TaskPeriod(DateTime start, DateTime stop) 
+        { 
+            DataStart = start;
+            DataStop = stop;
+        }
+
+    }
+}
